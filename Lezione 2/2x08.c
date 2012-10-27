833e20363ffa5aa512cca045eba560a7f02976c9
amandreamartino <amandreamartino@gmail.com>
2012-10-27 17:47:19 +0200

    Revert "Revert "Revert "Revert "Aggiunta mia versione""""
    This reverts commit 5bc05583b5d4a11f104d609513e01fb4bf57a92f.

--- Lezione 2/2x08.c
+++ Lezione 2/2x08.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -50,3 +51,41 @@ int main(){
   system("PAUSE>NUL");	
   return 0;
 }
+=======
+#include <stdio.h>
+
+int a, b, x, ntentativi, counter=1, trovato=0;  //contatore e variabile pseudo booleana che riferisce se il valore sia stata trovato o meno
+char direzione; //variabile char che contiene o il carattere s (per sinistra) o d (per destra)
+
+int main() {
+	
+	printf("Inserire valore a\n");
+	scanf("%d",&a);
+	printf("Inserire valore b\n");
+	scanf("%d",&b);
+	printf("Inserire valore da ricercare\n");
+	scanf("%d",&x);	
+	printf("Quanti tentativi?\n");
+	scanf("%d",&ntentativi);		
+	while (counter<=ntentativi && trovato==0) {  //il programma esce dal ciclo se una delle due condizioni cade
+		printf("Tentativo %d: cerco %d; a = %d; b = %d; (a+b)/2 = %d; ", counter, x, a, b, (a+b)/2);
+		if (x==(a+b)/2) {
+		   trovato=1;
+		   printf("trovato!\n");
+		}
+		else {
+		   if ((a+b)/2>x) {  //algoritmo di bisezione
+		      b=((a+b)/2);
+		      direzione='s';
+		   }
+		   else {
+		      a=((a+b)/2);
+		      direzione='d';
+		   }
+		   printf("non trovato; cerco a %cx\n", direzione);  //sx (se la variabile char direzione è = 's' o dx se uguale a 'd'
+		   counter++;
+		   }
+		}	
+	return 0;
+}
+>>>>>>> Aggiunto esercizio 8
