aa065e86f813dc1108eed42102708450257a4aeb
amandreamartino <amandreamartino@gmail.com>
2012-10-28 14:25:07 +0100

    Variabili double long

--- Esercizi a caso dell'Ondina/Fibonacci.c
+++ Esercizi a caso dell'Ondina/Fibonacci.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-unsigned double num1=0, num2=1;
+double long num1=0, num2=1;
 int counter=0;
 
 int main() 
@@ -8,16 +8,16 @@ int main()
 {
 	printf("Inserisci numero di cifre:");
 	scanf("%d",&counter);
-	printf("Successione di Fibonacci:\n %.0f ", num2);
+	printf("Successione di Fibonacci:\n%.0Lf ", num2);
 	while (counter>1)
 	  {
 		num1=num2+num1;
-		printf("%.0f ",num1);
+		printf("%.0Lf ",num1);
 		counter=counter-1;
 		  if (counter>2)
 		 {
 		       num2=num1+num2;
-		       printf("%.0f ",num2);
+		       printf("%.0Lf ",num2);
 		       counter=counter-1;
 		     }
 	  }
