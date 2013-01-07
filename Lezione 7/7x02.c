faf7105907d01a7186683aa2f6c0abe64d607fc8
Andrea Martino <amandreamartino@gmail.com>
2013-01-07 20:31:00 +0100

    Aggiunto esercizio 2

---
+++ Lezione 7/7x02.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#define DIM 20
+
+
+void copiafile (char orig[], char copia[]);
+
+int main () {
+
+    char nomeoriginale[DIM], nomecopia[DIM];
+
+    printf("Inserire nome stringa originale: ");
+    gets(nomeoriginale);
+    printf("Inserire nome stringa copia: ");
+    gets(nomecopia);
+    copiafile(nomeoriginale, nomecopia);
+}
+
+void copiafile (char orig[], char copia[]) {
+    FILE *fileorig, *filecopia;
+    char carattere;
+
+    //controlla apertura file
+    if ((fileorig=fopen(orig, "r"))==NULL) {
+        printf("Errore apertura file sorgente");
+        return;
+    }
+    if ((filecopia=fopen(copia, "w"))==NULL) {
+        printf("Errore apertura file copia");
+        return;
+    }
+    while (!feof(fileorig)) {
+        carattere=fgetc(fileorig); //dobbiamo lavorare per forza su una variabile. ogni chiamata di fgetc fa "avanzare lo stream"
+        if ((carattere>='A' && carattere<='Z') || (carattere>='a' && carattere<='z')) {
+            if (islower(carattere)) {
+                fputc(carattere-32, filecopia);
+            }
+            else {
+                fputc(carattere+32, filecopia);
+            }
+        }
+        else {
+        fputc(carattere, filecopia);
+        }
+    }
+    fclose(fileorig);
+    fclose(filecopia);
+}
